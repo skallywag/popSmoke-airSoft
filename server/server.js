1f6cfd760475307ba377335efd11758e44e64b94
Samuel Lee <lee.sam1492@gmail.com>
2024-01-24 14:19:29 -0700

    - created CreateGame.scss gam component - changed div to box on home component - installed react-hook-form -

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ import dotenv from "dotenv"
 import express from 'express'
 import cors from 'cors'
 
-// Controllers
 
 const app = express();
 dotenv.config()
 app.use(cors())
 app.use(express.json())
 
+// Controllers
 app.get('/mess', (req, res) => {
     console.log('hit')
     res.send({message: 'sent'});
