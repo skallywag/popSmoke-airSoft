3b5bf593b9bb6de8e84c2bde00ce936ac735992b
Samuel Lee <lee.sam1492@gmail.com>
2024-02-07 03:05:19 -0700

    - Formatted create game.tsx

--- src/pages/createGame/CreateGame.tsx
+++ src/pages/createGame/CreateGame.tsx
@@ -89,30 +89,62 @@ const CreateGame: React.FC = () => {
             <Box style={{boxShadow: '0 0 10px rgba(0,0,0,.18', padding: '36px 24px'}}>
                 <Title mb={20} size={18} style={{color: themeVars.primaryOrange}}>Operation</Title>
                 <form onSubmit={form.onSubmit(onSubmit)}>
-                    <TextInput mb={20} label="Title" placeholder="Title" {...form.getInputProps('title')}/>
-                    <Textarea mb={20} label="Description"
-                              placeholder="Description" {...form.getInputProps('description')}/>
-                    <Select mb={20} label="Game Type" placeholder="Select Game Type"
-                            data={["Open play", "Event"]} {...form.getInputProps('gameType')}/>
+                    <TextInput
+                        mb={20}
+                        label="Title"
+                        placeholder="Title"
+                        {...form.getInputProps('title')}/>
+                    <Textarea
+                        mb={20}
+                        label="Description"
+                        placeholder="Description"
+                        {...form.getInputProps('description')}/>
+                    <Select
+                        mb={20}
+                        label="Game Type"
+                        placeholder="Select Game Type"
+                        data={["Open play", "Event"]}
+                        {...form.getInputProps('gameType')}/>
 
                     {form.values.gameType === "Event" &&
-                        <Checkbox mb={20} onClick={() => handleResetEventValues()}
-                                  label={"Paid Event?"} {...form.getInputProps('paidEvent')}/>}
-
+                        <Checkbox
+                            mb={20}
+                            onClick={() => handleResetEventValues()}
+                            label={"Paid Event?"}
+                            {...form.getInputProps('paidEvent')}/>
+                    }
                     {form.values.paidEvent &&
                         <>
-                            <NumberInput ml={16} mb={20} placeholder="Entry Price"
-                                         prefix="$" {...form.getInputProps('entryPrice')}/>
-                            <MultiSelect ml={16} mb={20} placeholder="Select accepted payments"
-                                         data={["venmo", "paypal", "Cash"]} {...form.getInputProps('acceptedPayment')}/>
+                            <NumberInput
+                                ml={16}
+                                mb={20}
+                                placeholder="Entry Price"
+                                prefix="$"
+                                {...form.getInputProps('entryPrice')}/>
+                            <MultiSelect
+                                ml={16}
+                                mb={20}
+                                placeholder="Select accepted payments"
+                                data={["venmo", "paypal", "Cash"]}
+                                {...form.getInputProps('acceptedPayment')}/>
                         </>
                     }
-                    <Select mb={20} label="State" placeholder="Select State"
-                            data={["Arizona", "Utah"]} {...form.getInputProps('state')}/>
-                    <TextInput mb={20} label="Location Name"
-                               placeholder="Hobble Creek" {...form.getInputProps('locationName')}/>
-                    <TextInput mb={20} label="Address"
-                               placeholder="ie: 2248 Deer Crk Wy, Springville, UT 84663" {...form.getInputProps('address')}/>
+                    <Select
+                        mb={20}
+                        label="State"
+                        placeholder="Select State"
+                        data={["Arizona", "Utah"]}
+                        {...form.getInputProps('state')}/>
+                    <TextInput
+                        mb={20}
+                        label="Location Name"
+                        placeholder="Hobble Creek"
+                        {...form.getInputProps('locationName')}/>
+                    <TextInput
+                        mb={20}
+                        label="Address"
+                        placeholder="ie: 2248 Deer Crk Wy, Springville, UT 84663"
+                        {...form.getInputProps('address')}/>
                     <DateTimePicker
                         mb={30}
                         clearable
@@ -123,14 +155,22 @@ const CreateGame: React.FC = () => {
                     />
                     <Divider mb={20}/>
                     <Title mb={20} size={18} style={{color: themeVars.primaryOrange}}>Contact</Title>
-                    <TextInput mb={20} label="First Name"
-                               placeholder="First name" {...form.getInputProps('firstName')}/>
-
-                    <TextInput mb={20} label="Contact Email"
-                               placeholder="Contact Email" {...form.getInputProps('email')}/>
-
-                    <TextInput mb={20} label="Phone (Optional)" placeholder="Phone Number"
-                               type={"number"}{...form.getInputProps('phoneNumber')}/>
+                    <TextInput
+                        mb={20}
+                        label="First Name"
+                        placeholder="First name"
+                        {...form.getInputProps('firstName')}/>
+                    <TextInput
+                        mb={20}
+                        label="Contact Email"
+                        placeholder="Contact Email"
+                        {...form.getInputProps('email')}/>
+                    <TextInput
+                        mb={20}
+                        label="Phone (Optional)"
+                        placeholder="Phone Number"
+                        type={"number"}
+                        {...form.getInputProps('phoneNumber')}/>
 
                     <Button color={themeVars.primaryOrange} fullWidth mt={4} type='submit'>
                         Submit
